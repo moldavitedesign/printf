a6b4e62f8d79cd5ced5a3e9dffcbc40ff7f2e77a
Caleb Popoola <34130935+moldavitedesign@users.noreply.github.com>
2023-03-29 07:05:06 +0100

    Update precision.c

--- precision.c
+++ precision.c
@@ -2,7 +2,7 @@
 
 /**
  * get_precision - Calculates the precision for printing
- * by Joseph and Ciny
+ * by Caleb and Bosede
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
  * @list: list of arguments.
