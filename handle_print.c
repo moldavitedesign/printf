ad785e19e4ee4b1c449b841a08619a8712a79455
Caleb Popoola <34130935+moldavitedesign@users.noreply.github.com>
2023-03-29 07:03:50 +0100

    Update handle_print.c

--- handle_print.c
+++ handle_print.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * handle_print - Prints an argument based on its type
- * by Joseph and Ciny
+ * by Caleb and Bosede
  * @fmt: Formatted string in which to print the arguments.
  * @list: List of arguments to be printed.
  * @ind: ind.
