e6ba23df326e0eb46f070e0a0180a47b7bc9e410
Caleb Popoola <34130935+moldavitedesign@users.noreply.github.com>
2023-03-29 07:05:25 +0100

    Update print_functions.c

--- print_functions.c
+++ print_functions.c
@@ -4,7 +4,7 @@
 
 /**
  * print_char - Prints a char
- * by Joseph and Ciny
+ * by Caleb and Bosede
  * @types: List a of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
