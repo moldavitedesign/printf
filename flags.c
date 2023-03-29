80dc59300eacd8ea2ac8198cfc0597963bcceef4
Caleb Popoola <34130935+moldavitedesign@users.noreply.github.com>
2023-03-29 07:03:03 +0100

    Update flags.c

--- flags.c
+++ flags.c
@@ -2,7 +2,7 @@
 
 /**
  * get_flags - Calculates active flags
- * by Joseph and Ciny
+ * by Caleb and Bosede
  * @format: Formatted string in which to print the arguments
  * @i: take a parameter.
  * Return: Flags:
